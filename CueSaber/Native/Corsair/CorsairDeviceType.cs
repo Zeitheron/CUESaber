2edd194836c41515576677114db1c308e61b9c13
Zeitheron <zeitheron.tk@gmail.com>
2022-03-19 16:34:43 +0200

    Added missing device types for CUE SDK.

--- CueSaber/Native/Corsair/CorsairDeviceType.cs
+++ CueSaber/Native/Corsair/CorsairDeviceType.cs
@@ -10,12 +10,17 @@ namespace CUESaber.Native.Corsair
     /// </summary>
     public enum CorsairDeviceType
     {
-
         Unknown = 0,
         Mouse = 1,
         Keyboard = 2,
         Headset = 3,
         Mousemat = 4,
-        HeadsetStand = 5
+        HeadsetStand = 5,
+        CommanderPro = 6,
+        LightingNodePro = 7,
+        MemoryModule = 8,
+        Cooler = 9,
+        Motherboard = 10,
+        GraphicsCard = 11
     };
 }
